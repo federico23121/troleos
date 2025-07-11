99e99ca2e013cb60106fb2a9da300a60f2aad5d2
carlospalacios1234 <carlosramongui@gmail.com>
2025-07-10 21:50:30 -0300

    Update botsstefano.js

--- botsstefano.js
+++ botsstefano.js
@@ -4,7 +4,7 @@ puppeteer.use(StealthPlugin());
 
 // --- CONFIGURACIÓN ---
 const HAXBALL_ROOM_URL = process.env.HAXBALL_ROOM_URL; // Poné tu link
-const BOT_NICKNAME = process.env.JOB_ID || "bot";
+const BOT_NICKNAME = "figaln't" + process.env.JOB_ID || "bot";
 const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1393006720237961267/lxg_qUjPdnitvXt-aGzAwthMMwNbXyZIbPcgRVfGCSuLldynhFHJdsyC4sSH-Ymli5Xm"; // Tu webhook
 // ----------------------
 
