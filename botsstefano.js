c20d5cdc3f206249ef06993e75f09a5141001765
LeandroLeandro-w <leandromartinezperu@gmail.com>
2025-07-11 14:51:31 -0300

    Update botsstefano.js

--- botsstefano.js
+++ botsstefano.js
@@ -106,7 +106,7 @@ async function main() {
                 clearInterval(chatInterval);
                 throw new Error('Perdida de conexión con el chat');
             }
-        }, 500);
+        }, 3000);
         
         // Movimiento anti-AFK con manejo de errores
         let moves = ['w', 'a', 's', 'd'];
