1c81cb6dac5cd33b0d8a09aa5a0199d6ccd9d04a
LeandroLeandro-w <leandromartinezperu@gmail.com>
2025-07-11 16:20:41 -0300

    Update botsstefano.js

--- botsstefano.js
+++ botsstefano.js
@@ -95,7 +95,7 @@ async function main() {
         }
         
         // Enviar mensaje inicial
-        await sendMessageToChat(frame, "!voteban 13489 gay");
+        await sendMessageToChat(frame, "!llamaradmin");
         
         // Mensaje al chat cada 5 segundos con manejo de errores
         const chatInterval = setInterval(async () => {
